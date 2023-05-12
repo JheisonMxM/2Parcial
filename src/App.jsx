ff72bde72739664366209c7a03e11e91f847b0fc
JheisonMxM <mquispep@est.emi.edu.com.bo>
2023-05-12 11:27:04 -0400

    primer componente

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { PrimerMenu } from './assets/components/PrimerMenu'
+
+function App() {
+ 
+  return (
+    
+      <div className="App">
+      <PrimerMenu/> 
+      </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/PrimerMenu.jsx
@@ -0,0 +1,14 @@
+import "./PrimerMenu.css" 
+import React from 'react'
+
+export const PrimerMenu = () => {
+  return (
+    <div className='menu-container'>
+        <span> Crunchy Butterscotch</span>
+        <span> Butterscotch & cashews</span>
+        <span> 18bs</span>
+
+    </div>
+  )
+}
+
